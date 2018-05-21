60e74bff86c1c0fbd35cf5b7df9d930e0f74d1c6
Helena Rasche <hxr@hx42.org>
2018-05-21 11:24:06 +0200

    Remove old copy + switch to git deploy

--- files/runners/__init__.py
+++
@@ -1,9 +0,0 @@
-class Runner:
-    def custom_spec(self):
-        pass
-
-    def is_available(self):
-        return False
-
-    def reroute_to_dedicated(self, tool_spec, user_roles):
-        return {}
